1ff38684e30f23ddf7f061d564cd3c61e5902e10
dizehacioglu <dh1272@nyu.edu>
2015-07-29 16:06:51 -0600

    added form

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 $(document).on('ready', function() {
+
+	$('[name=current-income]').on('focusout', function(){
+		$('#c-i').text('Current income: ' + $(this).val());
+		$('#income-explanation').text('');
+	})
+	$('[name=current-taxes]').on('focusout', function(){
+		$('#c-i').text($(this).val());
+	})
+	$('[name=new-income]').on('focusout', function(){
+		$('#c-i').text($(this).val());
+	})
+	$('[name=new-taxes]').on('focusout', function(){
+		$('#c-i').text($(this).val());
+	})
+
+
+	$('[name=current-income]').on('focusin', function(){
+		$('#income-explanation').text('Go here');
+	})
+	$('[name=current-income]').on('focusin', function(){
+		$('#c-i').text($(this).val());
+	})
+	$('[name=current-income]').on('focusin', function(){
+		$('#c-i').text($(this).val());
+	})
+	$('[name=current-income]').on('focusin', function(){
+		$('#c-i').text($(this).val());
+	})
+
+
+
   
 });
